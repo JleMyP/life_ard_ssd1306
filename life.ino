6b71c913c2b724ddbae0484daa55583f7bcfab07
tim <timtim96@bk.ru>
2017-02-09 02:48:42 +0500

    small refactor

--- life.ino
+++ life.ino
@@ -1,75 +1,101 @@
 #include <Wire.h>
-#include "Adafruit_SSD1306.h"
-#include "Adafruit_GFX.h"
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
  
-#define map_w 128
-#define map_h 64
-#define size map_w*map_h
+ 
+ 
+const uint16_t map_w = 128;
+const uint16_t map_h = 64;
+const uint16_t size  = map_w * map_h;
+
+uint8_t surf[size]; // = calloc(size, 1);
+uint8_t nsurf[size]; // = calloc(size, 1);
+
 
 Adafruit_SSD1306 display(A2);
 
-bool surf[size];// = calloc(size, 1);
-bool nsurf[size];// = calloc(size, 1);
 
 
-
-
-void setup(){
+void setup() {
   randomSeed(analogRead(0));
   display.begin();
   generate();
 }
 
 
-void loop(){
+void loop() {
   step();
   draw();
 }
 
 
-void generate(){
-  for(uint16_t p=0; p<size; p++) surf[p] = random(2)?true:false;
+void generate() {
+  for (uint16_t p = 0; p < size; ++p) surf[p] = random(2);
 }
 
 
-void step(){
-  for(int16_t x=0; x<map_w; x++){
-    for(int16_t y=0; y<map_h; y++){
-      uint16_t pos = y*map_w+x;
-      byte sum = get_sum_mur(x, y);
-      if(sum == 3 or surf[pos] && sum == 2) nsurf[pos] = true;
-      else nsurf[pos] = false;
+void step() {
+  uint16_t x;
+  uint16_t y;
+  
+  uint16_t ypos;
+  uint16_t pos
+  
+  uint8_t sum;
+  
+  for (y = 0; y < map_h; ++y) {
+    ypos = y * map_w;
+    
+    for (x = 0; x < map_w; ++x) {
+      pos = ypos + x;
+      sum = get_sum_mur(x, y);
+      
+      nsurf[pos] = (sum == 3 or surf[pos] && sum == 2) nsurf[pos] = 1;
     }
   }
   
-  for(byte x=0; x<map_w; x++){
-    for(byte y=0; y<map_h; y++) surf[y*map_w+x] = nsurf[y*map_w+x];
+  for (y = 0; y < map_h; ++y) {
+    ypos = y * map_w;
+    
+    for (x = 0; y < map_w; ++x) surf[ypos + x] = nsurf[ypos + x];
   }
 }
 
 
-byte get_sum_mur(int16_t x, int16_t y){
-  byte sum = 0;
-  for(int16_t xx=x-1; xx<x+2; xx++){
-    for(int16_t yy=y-1; yy<y+2; yy++) sum += (byte)get(xx, yy);
+uint8_t get_sum_mur(int16_t x, int16_t y) {
+  int16_t xx;
+  int16_t yy;
+  
+  uint8_t sum = 0;
+  
+  for (xx = x - 1; xx < x + 2; ++xx) {
+    for (yy = y - 1; yy < y + 2; ++yy) sum += get(xx, yy);
   }
+  
   return sum;
 }
 
-bool get(int16_t x, int16_t y){
-  if(x == map_w) x = 0;
-  else if(x == -1) x = map_w-1;
-  if(y == map_h) y = 0;
-  else if(y == -1) y = map_h-1;
-  return surf[y*map_w+x];
+uint8_t get(int16_t x, int16_t y) {
+  if (x == map_w) x = 0;
+  else if (x == -1) x = map_w - 1;
+  
+  if (y == map_h) y = 0;
+  else if (y == -1) y = map_h - 1;
+  
+  return surf[y * map_w + x];
 }
 
 
-void draw(){
-  for(int16_t x=0; x<map_w; x++){
-    for(int16_t y=0; y<map_h; y++){
-      display.drawPixel(x, y, surf[y*map_w+x]);
-    }
+void draw() {
+  uint16_t x;
+  uint16_t y;
+  uint16_t ypos;
+  
+  for (y = 0; y < map_h; ++y) {
+    ypos = y * map_w;
+  
+    for (x = 0; x < map_w; ++x) display.drawPixel(x, y, surf[ypos + x]);
   }
+  
   display.display();
 }
